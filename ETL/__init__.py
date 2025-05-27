3184eb06a3086148c56647226a7540cdb0d9e8e8
Kharytia <150616662+Kharytia@users.noreply.github.com>
2025-05-27 19:54:46 +0200

    Update ETL __init__ with RFM, churn, sales analysis, market_basket and clv functions

--- ETL/__init__.py
+++ ETL/__init__.py
@@ -1,3 +1,14 @@
 from .extract import load_data
 from .transform import clean_data, add_return_flag, split_sales_returns
 from .load import save_to_csv
+from .rfm import calculate_rfm, assign_rfm_scores, score_rfm, assign_customer_segments
+from .churn import calculate_customer_metrics, classify_churn_status, get_churn_summary
+from .sales_analysis import (
+    sales_over_time,
+    top_selling_products,
+    product_returns,
+    country_orders,
+    country_revenue
+)
+from .market_basket import prepare_basket_data, generate_association_rules, get_top_rules
+from .clv import estimate_clv, calculate_customer_metrics, simulate_churn_loss
