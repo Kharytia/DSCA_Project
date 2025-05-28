b2900f0cef112819abdeb16698e882b3a3987b64
Kharytia <150616662+Kharytia@users.noreply.github.com>
2025-05-28 15:54:19 +0200

    Add new functions to main

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ from utils.plotting import (
     plot_country_revenue,
     plot_product_returns,
     plot_sales_over_time,
-    plot_top_selling_products
+    plot_top_selling_products,
+    plot_country_orders
 )
 
 # Load and clean data
@@ -38,45 +39,46 @@ df = clean_data(df)
 df = add_return_flag(df)
 sales, returns = split_sales_returns(df)
 
-# --- RFM Analysis ---
+# RFM Analysis 
 rfm = calculate_rfm(sales)
 rfm = assign_rfm_scores(rfm)
 rfm = assign_customer_segments(rfm)
-plot_rfm_segments(rfm)
-plt.savefig("visualizations/rfm_segments.png")
-plot_frequency_distribution(rfm)
-plt.savefig("visualizations/frequency_distribution.png")
-plot_revenue_by_segment(sales, rfm)
-plt.savefig("visualizations/revenue_by_segment.png")
+plot_rfm_segments(rfm,"visualizations/rfm_segments.png" )
+plot_frequency_distribution(rfm, "visualizations/frequency_distribution.png")
+plot_revenue_by_segment(rfm, "visualizations/revenue_by_segment.png")
 
-# --- Churn Analysis ---
+
+# Churn Analysis
 churn_df = calculate_customer_metrics(sales)
 churn_df = classify_churn_status(churn_df)
-plot_churn_distribution(churn_df)
-plt.savefig("visualizations/churn_distribution.png")
+churn_df_summary = get_churn_summary(churn_df)
+plot_churn_distribution(churn_df_summary, "visualizations/churn_distribution.png")
 
-# --- Sales Trends ---
-plot_sales_over_time(df)
-plt.savefig("visualizations/sales_trends.png")
-plot_product_returns(df)
-plt.savefig("visualizations/returns_trends.png")
-plot_top_selling_products(df)
-plt.savefig("visualizations/top_products.png")
-plot_country_revenue(df)
-plt.savefig("visualizations/country_revenue.png")
+# Sales Trends 
+monthly_sales = sales_over_time(sales)
+top_products = top_selling_products(sales)
+top_returns = product_returns(returns)
+country_sales = country_revenue(sales)
+orders_country = country_orders(sales)
 
-# --- Market Basket ---
-basket_df = prepare_basket_data(sales)
-rules = generate_association_rules(basket_df)
+plot_sales_over_time(monthly_sales, "visualizations/sales_trends.png")
+plot_product_returns(top_returns, "visualizations/returns_trends.png")
+plot_top_selling_products(top_products, "visualizations/top_products.png")
+plot_country_revenue(country_sales, "visualizations/country_revenue.png")
+plot_country_orders(orders_country, "visualizations/country_orders.png")
+
+# Market Basket
+basket = prepare_basket_data(df)
+rules = generate_association_rules(basket)
 top_rules = get_top_rules(rules)
-plot_association_rules(top_rules)
-plt.savefig("visualizations/item_combinations.png")
+
+plot_association_rules(rules, "visualizations/association_rules.png", top_n=10)
+
 
 # --- CLV ---
 clv_df = calculate_customer_metrics(sales)
 clv_df = estimate_clv(clv_df)
 loss = simulate_churn_loss(clv_df, churn_df)
-plot_clv_distribution(clv_df)
-plt.savefig("visualizations/clv_distribution.png")
+plot_clv_distribution(clv_df, "visualizations/clv_distribution.png")
 
 print(f"Estimated churn loss: £{loss:,.2f}")
